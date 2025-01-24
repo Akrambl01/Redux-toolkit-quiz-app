c23268d22aee74bd3c1b60dda73162e69a159fba
akram <akramibnelyazid@gmail.com>
2025-01-24 03:18:41 +0100

    refactor: update Progress component and localize questions in JSON

--- src/Progress.jsx
+++ src/Progress.jsx
@@ -3,21 +3,21 @@ import { useSelector } from "react-redux";
 function Progress() {
   const questions = useSelector((state) => state.questions);
   const index = useSelector((state) => state.index);
-  const points = useSelector((state) => state.points);
+  // const points = useSelector((state) => state.points);
   const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
+  // const maxPossiblePoints = questions.reduce(
+  //   (prev, cur) => prev + cur.points,
+  //   0
+  // );
   return (
     <header className="progress">
       <progress max={numQuestions} value={index + 1}></progress>
       <p>
         Question <strong>{index + 1}</strong> / {numQuestions}{" "}
       </p>
-      <p>
+      {/* <p>
         <strong>{points}</strong> / {maxPossiblePoints}
-      </p>
+      </p> */}
     </header>
   );
 }
